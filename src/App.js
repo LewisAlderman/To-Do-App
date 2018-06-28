7e86695fe422fba90c4bbbbadfe7fd23295b9348
Lewis_A <lewisalderman95@gmail.com>
2018-06-28 15:14:40 +0100

    Added redux, react-redux, Created store (reducer, inital)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import initial from "./data/initial";
+import reducer from "./data/reducer";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const store = createStore(reducer, initial);
+
+const App = () => (
+	<Provider store={store}>
+		<p>App</p>
+	</Provider>
+);
 
 export default App;
