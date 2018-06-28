ed283f85dde376383a233ff49de9414f14101b35
Lewis_A <lewisalderman95@gmail.com>
2018-06-28 14:19:18 +0100

    Revert "working input, list adding item"
    This reverts commit c2e3089253b91b1626ef5a097c2879838efba6f5.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
-import List from "./components/List";
 
 class App extends Component {
 	render() {
-		return (
-			<React.Fragment>
-				<Header title="To-Do List" />
-				<List />
-			</React.Fragment>
-		);
+		return <Header title="To-Do List" />;
 	}
 }
 
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,78 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-class List extends Component {
-	constructor(props) {
-		super(props);
+const List = ({tasks}) => (
 
-		this.state = {
-			toDoArray: [
-				{
-					id: 1,
-					task: "finish to do app",
-					completed: false,
-				},
-				{
-					id: 2,
-					task: "edited task",
-					completed: true,
-				},
-				{
-					id: 3,
-					task: "buy biscuits",
-					completed: false,
-				},
-				{
-					id: 4,
-					task: "wash car",
-					completed: true,
-				},
-			],
-			listItem: "",
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleClick = this.handleClick.bind(this);
-	}
+    <React.Fragment>
 
-	handleChange(e) {
-		this.setState({ listItem: e.target.value });
-	}
+    { tasks.length ? <ul className="list-group">/<ul>
+    { tasks.map(task => (
+        <li className="list-group-item" key= {task.id}>
+            
+    )) }
+    }
 
-	handleClick() {
-		let { toDoArray } = this.state;
-		const input = document.getElementById("inputfield");
-		input.value = "";
-		this.setState({
-			toDoArray: [
-				...toDoArray,
-				{
-					id: 5,
-					task: this.state.listItem,
-					completed: false,
-				},
-			],
-			listItem: input.value,
-		});
-	}
+    </React.Fragment>
 
-	render() {
-		const { toDoArray } = this.state;
-		return (
-			<React.Fragment>
-				<p>Add a task</p>
-				<input id="inputfield" onChange={this.handleChange} type="text" />
-				<button onClick={this.handleClick}>Add</button>
-				{!toDoArray.length ? (
-					<p>Not items here</p>
-				) : (
-					<ul>
-						{" "}
-						{this.state.toDoArray.map((item, index) => (
-							<li key={index}> {item.task} </li>
-						))}
-					</ul>
-				)}
-			</React.Fragment>
-		);
-	}
-}
+);
+
+
+
 export default List;
